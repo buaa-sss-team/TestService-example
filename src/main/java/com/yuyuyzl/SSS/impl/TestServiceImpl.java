52e62f8d9c3f7c672deda1a042e1d760184643b9
yuyuyzl <yuzhongliang22@163.COM>
2019-05-08 08:45:00 +0800

    逐渐example化

--- src/main/java/com/yuyuyzl/SSS/impl/TestServiceImpl.java
+++ src/main/java/com/yuyuyzl/SSS/impl/TestServiceImpl.java
@@ -11,12 +11,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+//TODO 修改这里的标注为实际服务REF，与spring-provider.xml中的ref相同
 @Service("TestService")
+//TODO 修改类名和接口引用为合适的类名
 public class TestServiceImpl implements ITestService {
 
     private Logger logger = LoggerFactory.getLogger(TestServiceImpl.class);
 
-
+    //TODO 你的接口实现↓
     public boolean login(String username, String password) {
         logger.info("用户登录：[username:{}, password:{}]", username, password);
         if (username != null && password != null && username.equals(password)) {
@@ -41,4 +43,6 @@ public class TestServiceImpl implements ITestService {
         return new MyReturnClass(lm);
 
     }
+
+    //TODO 你的接口实现↑
 }
